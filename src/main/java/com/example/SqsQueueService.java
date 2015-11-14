69e735a2485cf0d3ae7ec3ead522afaa7f0bfb23
Huanhuan Huang <huanhuan.huang@betterbills.com>
2015-11-14 16:16:28 +1100

    added implementation of sqsQueueService

--- src/main/java/com/example/SqsQueueService.java
+++ src/main/java/com/example/SqsQueueService.java
@@ -1,10 +1,15 @@
 package com.example;
 
 import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.DeleteMessageRequest;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
 
+import java.util.List;
 import java.util.Queue;
 
 public class SqsQueueService implements QueueService {
+  private AmazonSQSClient sqs;
   //
   // Task 4: Optionally implement parts of me.
   //
@@ -14,17 +19,23 @@ public class SqsQueueService implements QueueService {
   //
 
   public SqsQueueService(AmazonSQSClient sqsClient) {
+    this.sqs = sqsClient;
   }
 
   public Message pull(Queue<Message> queue) {
+    ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest("myQueueUrl");
+    List<com.amazonaws.services.sqs.model.Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+    messages.get(0).getBody();
     return null;
   }
 
   public boolean push(String message, Queue<Message> queue) {
-    return true;
+    sqs.sendMessage(new SendMessageRequest("myQueueUrl", message));
+    return false;
   }
 
   public boolean delete(Message message, Queue<Message> queue) {
+    sqs.deleteMessage(new DeleteMessageRequest("myQueueUrl", "messageRecieptHandle"));
     return false;
   }
 }
